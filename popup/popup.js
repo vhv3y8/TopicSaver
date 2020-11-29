a6d43ddb6ae46e85ea8c2611d32749e23e1baa5b
vhv3y8 <vhv3y8@gmail.com>
2020-11-29 20:56:39 +0900

    adding functions 1

--- popup/popup.js
+++ popup/popup.js
@@ -97,109 +97,6 @@ let createFolderBar = function (folderStack) {
   return bar;
 };
 
-// functions mapping json data into html element
-let createFolderItem = function (folderData) {
-  //  Data Format
-  //  let folderData = {
-  //     name: "folderName",
-  //     datas: {
-  //       folders: [],
-  //       links: [],
-  //     },
-  //   },
-  let element = document.createElement("div");
-  element.setAttribute("class", "item");
-
-  // add Events
-  let defaultColor;
-  element.addEventListener("mouseover", function () {
-    defaultColor = element.style.backgroundColor;
-    element.style.backgroundColor = "#E6E6E6";
-    element.style.cursor = "pointer";
-  });
-  element.addEventListener("mouseout", function () {
-    element.style.backgroundColor = defaultColor;
-  });
-
-  // add img
-  let icon = document.createElement("img");
-  icon.setAttribute("alt", "Folder");
-  icon.setAttribute("src", "../assets/icons/iconmonstr-folder-2-16.png");
-
-  // add text
-  let text = document.createElement("p");
-  text.innerHTML = folderData.name;
-
-  element.appendChild(icon);
-  element.appendChild(text);
-
-  // console.log(element);
-
-  return element;
-};
-let createURLItem = function (urlData) {
-  // Data Format
-  // let urlData = {
-  //   title: "MDN reference",
-  //   url: "https://www.google.com",
-  // }
-  let element = document.createElement("div");
-  element.setAttribute("class", "item");
-
-  // add Events
-  let defaultColor;
-  element.addEventListener("mouseover", function () {
-    defaultColor = element.style.backgroundColor;
-    element.style.backgroundColor = "#E6E6E6";
-    element.style.cursor = "pointer";
-  });
-  element.addEventListener("mouseout", function () {
-    element.style.backgroundColor = defaultColor;
-  });
-
-  // add img
-  let icon = document.createElement("img");
-  icon.setAttribute("alt", "URL");
-  icon.setAttribute("src", "../assets/icons/iconmonstr-link-7-12.png");
-  icon.style.width = "12px";
-  icon.style.height = "12px";
-
-  let text = document.createElement("p");
-  text.innerHTML = urlData.title;
-
-  [icon, text].forEach((x) => element.appendChild(x));
-  // element.appendChild(icon);
-  // element.appendChild(text);
-
-  // console.log(element);
-
-  return element;
-};
-
-//  ????
-let createAddSelectItem = function () {
-  let element = document.createElement("div");
-  element.setAttribute("class", "item");
-  element.setAttribute("id", "addSelect"); // id="addSelect"
-
-  // add Events
-  let defaultColor;
-  element.addEventListener("mouseover", function () {
-    defaultColor = element.style.backgroundColor;
-    element.style.backgroundColor = "#E6E6E6";
-    element.style.cursor = "pointer";
-  });
-  element.addEventListener("mouseout", function () {
-    element.style.backgroundColor = defaultColor;
-  });
-
-  let createSelection = function () {
-    let element = document.createElement("div");
-  };
-
-  return element;
-};
-
 // object that contain functions about sorting list items.
 let sorter = {
   // sort functions
@@ -235,6 +132,128 @@ let sorter = {
   },
 };
 
+let folderRouter = {
+  // functions mapping json data into html element
+  createFolderItem: function (folderData) {
+    //  Data Format
+    //  let folderData = {
+    //     name: "folderName",
+    //     datas: {
+    //       folders: [],
+    //       links: [],
+    //     },
+    //   },
+    let element = document.createElement("div");
+    element.setAttribute("class", "item");
+
+    // add Events
+    let defaultColor;
+    element.addEventListener("mouseover", function () {
+      defaultColor = element.style.backgroundColor;
+      element.style.backgroundColor = "#E6E6E6";
+      element.style.cursor = "pointer";
+    });
+    element.addEventListener("mouseout", function () {
+      element.style.backgroundColor = defaultColor;
+    });
+
+    // add img
+    let icon = document.createElement("img");
+    icon.setAttribute("alt", "Folder");
+    icon.setAttribute("src", "../assets/icons/iconmonstr-folder-2-16.png");
+
+    // add text
+    let text = document.createElement("p");
+    text.innerHTML = folderData.name;
+
+    element.appendChild(icon);
+    element.appendChild(text);
+
+    // console.log(element);
+
+    return element;
+  },
+  createURLItem: function (urlData) {
+    // Data Format
+    // let urlData = {
+    //   title: "MDN reference",
+    //   url: "https://www.google.com",
+    // }
+    let element = document.createElement("div");
+    element.setAttribute("class", "item");
+
+    // add Events
+    let defaultColor;
+    element.addEventListener("mouseover", function () {
+      defaultColor = element.style.backgroundColor;
+      element.style.backgroundColor = "#E6E6E6";
+      element.style.cursor = "pointer";
+    });
+    element.addEventListener("mouseout", function () {
+      element.style.backgroundColor = defaultColor;
+    });
+
+    // add img
+    let icon = document.createElement("img");
+    icon.setAttribute("alt", "URL");
+    icon.setAttribute("src", "../assets/icons/iconmonstr-link-7-12.png");
+    icon.style.width = "12px";
+    icon.style.height = "12px";
+
+    let text = document.createElement("p");
+    text.innerHTML = urlData.title;
+
+    [icon, text].forEach((x) => element.appendChild(x));
+    // element.appendChild(icon);
+    // element.appendChild(text);
+
+    // console.log(element);
+
+    return element;
+  },
+
+  //  ????
+  createAddSelectItem: function () {
+    let element = document.createElement("div");
+    element.setAttribute("class", "item");
+    element.setAttribute("id", "addSelect"); // id="addSelect"
+
+    // add Events
+    let defaultColor;
+    element.addEventListener("mouseover", function () {
+      defaultColor = element.style.backgroundColor;
+      element.style.backgroundColor = "#E6E6E6";
+      element.style.cursor = "pointer";
+    });
+    element.addEventListener("mouseout", function () {
+      element.style.backgroundColor = defaultColor;
+    });
+
+    let createSelection = function () {
+      let element = document.createElement("div");
+    };
+
+    return element;
+  },
+
+  openFolder: function (folderObj, folderStack) {
+    // Data Format
+    // let folderObj = {
+    //   name: "folderName",
+    //   datas: {
+    //     folders: [],
+    //     links: [],
+    //   },
+    // };
+    // let folderStack = ["Main", "JavaScript", "Chrome Extension"];
+    // folderObj is db.folders.~~~~
+
+    document.querySelector("article section").remove(); // remove remaining list
+    let list = document.createElement("section");
+    let folderBar = document.getElementById("folderBar");
+  },
+};
+
 // runs when popup page is opened
 window.onload = async (e) => {
   console.log("Window Successfully loaded.");
@@ -256,10 +275,10 @@ window.onload = async (e) => {
   // Add data element into list
   let list = document.querySelector("article section");
   for (let i = 0; i < db.folders.length; i++) {
-    list.appendChild(createFolderItem(db.folders[i]));
+    list.appendChild(folderRouter.createFolderItem(db.folders[i]));
   }
   for (let i = 0; i < db.links.length; i++) {
-    list.appendChild(createURLItem(db.links[i]));
+    list.appendChild(folderRouter.createURLItem(db.links[i]));
   }
 
   // let colorChange = function (color) {
@@ -272,27 +291,6 @@ window.onload = async (e) => {
   // colorChange(colorwas);
 
   console.log(document.body.style.backgroundColor);
-};
-
-let folderRouter = function (folderObj, folderStack) {
-  let openFolder = function (folderObj, folderStack) {
-    // Data Format
-    // let folderObj = {
-    //   name: "folderName",
-    //   datas: {
-    //     folders: [],
-    //     links: [],
-    //   },
-    // };
-    // let folderStack = ["Main", "JavaScript", "Chrome Extension"];
-    // folderObj is db.folders.~~~~
-
-    document.querySelector("article section").remove(); // remove remaining list
-    let list = document.createElement("section");
-    let folderBar = document.getElementById("folderBar");
-  };
-
-  let;
 };
 
 // Adding events
